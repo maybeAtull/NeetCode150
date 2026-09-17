4c5caf655f5314759dbd28bde1bfbd963bb808cd
maybeAtull <atulgaurav786@gmail.com>
2026-09-17 12:18:23 +0530

    242 Valid anagram

---
+++ Array&Hashing/ValidAnagram.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class ValidAnagram {
+        public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        return Arrays.equals(sArray, tArray);
+
+    }
+    public static void main(String[] args) {
+        ValidAnagram ob = new ValidAnagram();
+        String s = "cat";
+        String t = "tacu";
+        Boolean Res = ob.isAnagram(s,t);
+        System.out.println(Res);
+    }
+}
